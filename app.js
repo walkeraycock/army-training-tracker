fadef124161a4d5ec3bc209dd7fac108c84f8ba9
walkeraycock <walker.aycock@gmail.com>
2026-06-15 18:45:11 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,3 @@
+const STORAGE_KEY="walker_training_v3";
+const trainingPlan={1:{intervals:"6 x 400m",tempo:"2 Mile Tempo",longRun:"3 Miles"},2:{intervals:"8 x 400m",tempo:"2.5 Mile Tempo",longRun:"3.5 Miles"},3:{intervals:"5 x 800m",tempo:"3 Mile Tempo",longRun:"4 Miles"},4:{intervals:"6 x 800m",tempo:"3 Mile Tempo",longRun:"5 Miles"},5:{intervals:"4 x 1200m",tempo:"4 Mile Tempo",longRun:"5-6 Miles"},6:{intervals:"6 x 400m",tempo:"2 Mile Easy",longRun:"2 Mile Time Trial"}};
+console.log("Army Tracker V3 loaded");
